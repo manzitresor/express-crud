5c2f90482ddd07a2bcf5a50b57185c9dbce62932
manzitresor <manzitre@gmail.com>
2024-09-13 14:45:24 +0200

    chore: setup view using swagger

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-const express = require('express');
-const itemsRouter = require('./routes/items.routes')
-const app = express();
 const dontenv = require('dotenv');
-dontenv.config();
+const swaggerUi = require('swagger-ui-express')
+const express = require('express');
+const itemsRouter = require('./routes/items.routes');
+const swaggerUiDocs = require('./views/swaggerUiDocs')
 
+const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.json())
+dontenv.config();
+app.use('/api/docs',swaggerUi.serve);
+app.use('/api/docs',swaggerUi.setup(swaggerUiDocs));
+app.use(express.json());
+app.use('/api',itemsRouter);
 
-app.use('/api',itemsRouter)
-console.log(process.env.PORT)
 
-app.listen(port,()=> console.log(`Listening on PORT ${port}`))
+app.listen(port,()=> console.log(`Listening on PORT ${port}`));
---
+++ views/swaggerUiDocs.js
@@ -0,0 +1,7 @@
+
+
+const swaggerUiDocs = {
+    openapi: '3.0.0'
+}
+
+module.exports = swaggerUiDocs
