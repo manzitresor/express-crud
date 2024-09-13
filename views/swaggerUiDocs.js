abb4555ce30a0c8913217836a6db6feb436f91ae
manzitresor <manzitre@gmail.com>
2024-09-13 14:55:46 +0200

    chore: setup servers and tags in swagger doc

--- views/swaggerUiDocs.js
+++ views/swaggerUiDocs.js
@@ -1,7 +1,24 @@
 
 
 const swaggerUiDocs = {
-    openapi: '3.0.0'
+    openapi: '3.0.0',
+    info: {
+     title: "Crud API",
+     version: "1.0.0",
+     description: "Simple API that handles basic Node.js server using Express"
+    },
+    servers: [
+        {
+            url: "http://localhost:3000",
+            description: "Local server"
+        }
+    ],
+    tags: [
+        {
+            name: "Items"
+        }
+    ],
+    
 }
 
 module.exports = swaggerUiDocs
